d972e9b18378f8665c04c46eddb7f656ce8327c1
kamalakar chitla <kamalakarchitla@gmail.com>
2026-06-23 18:11:56 +0530

    render database config

--- storebackend/backend/config/db.js
+++ storebackend/backend/config/db.js
@@ -1,10 +1,29 @@
+// import mysql from "mysql2";
+
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "root123",
+//   database: "store_rating_db"
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.log("DB Error:", err);
+//   } else {
+//     console.log("MySQL Connected");
+//   }
+// });
+
+// export default db;
 import mysql from "mysql2";
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root123",
-  database: "store_rating_db"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect((err) => {
